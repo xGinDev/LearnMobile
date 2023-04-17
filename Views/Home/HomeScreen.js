f6efb1f592832203028c8c4c7d44989d2276e4d2
xGinDev <jecl2912@gmail.com>
2023-04-17 16:38:17 -0500

    feat: logo home

--- Views/Home/HomeScreen.js
+++ Views/Home/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Pressable, View, Text, StyleSheet, Button } from 'react-native'
+import { Pressable, View, Text, StyleSheet, Button, Image, ScrollView } from 'react-native'
+import Logo from '../../src/images/gin.jpg'
 
 const HomeScreen = ({ navigation }) => {
   const [drawer, setDrawer] = useState(false);
@@ -14,19 +15,22 @@ const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={style.titleWelcome}>Bienvenidos a la app de xGinDev</Text>
+      <View style={style.containerImg}><Image style={style.imgLogo} source={Logo} /></View>
       <Button title={drawer ? 'Cerrar Menú' : 'Abrir Menú'} color='#CDA434' onPress={() => (handleDrawerHome(), handleCloseDrawerHome())} />
       {
         drawer ?
-          <View style={style.containerMenu}>
-            <Pressable style={[style.buttonContact, style.button]} onPress={() => navigation.navigate('Contact')}>
-              <Text style={style.textButton}>Ir a contacto</Text>
-            </Pressable>
-            <Pressable style={[style.buttonCount, style.button]} onPress={() => navigation.navigate('CountScreen')}>
-              <Text style={style.textButton}>Ir a contador</Text>
-            </Pressable>
-            <Pressable style={[style.buttonModal, style.button]} onPress={() => navigation.navigate('Modal')}>
-              <Text style={style.textButton}>Ir a Modal</Text>
-            </Pressable>
+          <View>
+            <ScrollView style={style.containerMenu}>
+              <Pressable style={[style.buttonContact, style.button]} onPress={() => navigation.navigate('Contact')}>
+                <Text style={style.textButton}>Ir a contacto</Text>
+              </Pressable>
+              <Pressable style={[style.buttonCount, style.button]} onPress={() => navigation.navigate('CountScreen')}>
+                <Text style={style.textButton}>Ir a contador</Text>
+              </Pressable>
+              <Pressable style={[style.buttonModal, style.button]} onPress={() => navigation.navigate('Modal')}>
+                <Text style={style.textButton}>Ir a Modal</Text>
+              </Pressable>
+            </ScrollView>
           </View>
           : null
       }
@@ -41,7 +45,6 @@ const style = StyleSheet.create({
     textAlign: 'center'
   },
   containerMenu: {
-    gap: 10,
     padding: 10
   },
   button: {
@@ -51,7 +54,8 @@ const style = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: '#000'
+    backgroundColor: '#000',
+    marginBottom: 10
   },
   textButton: {
     fontSize: 16,
@@ -59,6 +63,16 @@ const style = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: 'white',
+  },
+  containerImg: {
+    alignItems: 'center'
+  },
+  imgLogo: {
+    resizeMode: 'cover',
+    width: 200,
+    height: 200,
+    borderRadius: 500,
+    marginBottom: 20
   }
 })
 
